a51d19b9b89d20a02d3f278cae9c62e3127c5541
Diego Pereira <francisco.gamileira@accenture.com>
2025-04-17 10:31:22 -0300

    ajusted content

--- web/src/pages/Main/index.tsx
+++ web/src/pages/Main/index.tsx
@@ -12,23 +12,21 @@ type Post = {
 }
 
 
-// Função para salvar posts no localStorage
 const savePosts = (posts: Post[]) => {
   localStorage.setItem('posts', JSON.stringify(posts))
 }
 
-// Função para gerar a senha com base na hora atual
 const generatePassword = (): string => {
   const now = new Date()
-  const hours = now.getHours().toString().padStart(2, '0') // Ex: "17"
-  const minutes = now.getMinutes().toString().padStart(2, '0') // Ex: "21"
+  const hours = now.getHours().toString().padStart(2, '0')
+  const minutes = now.getMinutes().toString().padStart(2, '0')
 
-  const h1 = parseInt(hours[0]) + parseInt(hours[1]) // 1 + 7 = 8
-  const m1 = parseInt(minutes[0]) + parseInt(minutes[1]) // 2 + 1 = 3
-  const h2 = parseInt(hours[1]) + parseInt(minutes[0]) // 7 + 2 = 9
-  const m2 = parseInt(hours[0]) + parseInt(minutes[1]) // 1 + 1 = 2
+  const h1 = parseInt(hours[0]) + parseInt(hours[1])
+  const m1 = parseInt(minutes[0]) + parseInt(minutes[1])
+  const h2 = parseInt(hours[1]) + parseInt(minutes[0])
+  const m2 = parseInt(hours[0]) + parseInt(minutes[1])
 
-  return `${h1}${m1}${h2}${m2}` // Ex: "8339"
+  return `${h1}${m1}${h2}${m2}`
 }
 
 const PasswordModal = ({
@@ -79,17 +77,15 @@ const Main = () => {
   const [isDarkMode, setIsDarkMode] = useState(false)
   const [searchQuery, setSearchQuery] = useState('')
   const [selectedPost, setSelectedPost] = useState<Post | null>(null)
-  const [newPost, setNewPost] = useState({ title: '', description: '', contentHtml: '' })
+  const [post, setPost] = useState<Post>({ id: 0, title: '', description: '', contentHtml: '', createdAt: new Date(), updatedAt: new Date() })
   const [passwordInput, setPasswordInput] = useState('')
   const [postToDelete, setPostToDelete] = useState<number | null>(null)
-  const [postToEdit, setPostToEdit] = useState<Post | null>(null)
 
   const [isPasswordModalForNewPostOpen, setIsPasswordModalForNewPostOpen] = useState(false)
   const [isPasswordModalForEditOpen, setIsPasswordModalForEditOpen] = useState(false)
   const [isPasswordModalForDeleteOpen, setIsPasswordModalForDeleteOpen] = useState(false)
 
-  const [isNewPostModalOpen, setIsNewPostModalOpen] = useState(false)
-  const [isEditModalOpen, setIsEditModalOpen] = useState(false)
+  const [isPostModalOpen, setIsPostModalOpen] = useState(false)
 
   const loadPosts = async (): Promise<Post[]> => {
     const response = await fetch('https://chaplet-divine-mercy.onrender.com/posts');
@@ -116,9 +112,9 @@ const Main = () => {
   const handleCreatePost = async () => {
     const newPostData: Post = {
       id: Date.now(),
-      title: newPost.title,
-      description: newPost.description,
-      contentHtml: newPost.contentHtml,
+      title: post.title,
+      description: post.description,
+      contentHtml: post.contentHtml,
       createdAt: new Date(),
       updatedAt: new Date(),
     };
@@ -132,29 +128,29 @@ const Main = () => {
     if (response.ok) {
       const createdPost = await response.json();
       setPosts([...posts, createdPost]);
-      setNewPost({ title: '', description: '', contentHtml: '' });
-      setIsNewPostModalOpen(false);
+      setPost({ id: 0, title: '', description: '', contentHtml: '', createdAt: new Date(), updatedAt: new Date() });
+      setIsPostModalOpen(false);
     } else {
       alert('Erro ao criar o post.');
     }
   };
 
   const handleSaveEditedPost = async () => {
-    if (postToEdit) {
+    if (post) {
       try {
-        const response = await fetch(`https://chaplet-divine-mercy.onrender.com/posts/${postToEdit.id}`, {
+        const response = await fetch(`https://chaplet-divine-mercy.onrender.com/posts/${post.id}`, {
           method: 'PUT',
           headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(postToEdit),
+          body: JSON.stringify(post),
         });
 
         if (response.ok) {
           const updatedPosts = posts.map(post =>
-            post.id === postToEdit.id ? postToEdit : post
+            post.id === post.id ? post : post
           );
           setPosts(updatedPosts);
-          setIsEditModalOpen(false);
-          setPostToEdit(null);
+          setIsPostModalOpen(false);
+          setPost({ id: 0, title: '', description: '', contentHtml: '', createdAt: new Date(), updatedAt: new Date() });
         } else {
           alert('Erro ao salvar as alterações.');
         }
@@ -185,7 +181,7 @@ const Main = () => {
   };
 
   const handleOpenEditPasswordModal = (post: Post) => {
-    setPostToEdit(post);
+    setPost(post);
     setIsPasswordModalForEditOpen(true);
   };
 
@@ -199,7 +195,7 @@ const Main = () => {
     if (passwordInput === generatedPassword) {
       setIsPasswordModalForNewPostOpen(false);
       setPasswordInput('');
-      setIsNewPostModalOpen(true);
+      setIsPostModalOpen(true);
     } else {
       alert('Senha incorreta!');
     }
@@ -210,7 +206,7 @@ const Main = () => {
     if (passwordInput === generatedPassword) {
       setIsPasswordModalForEditOpen(false);
       setPasswordInput('');
-      setIsEditModalOpen(true);
+      setIsPostModalOpen(true);
     } else {
       alert('Senha incorreta!');
     }
@@ -257,7 +253,6 @@ const Main = () => {
         </div>
       </div>
 
-      {/* Content */}
       <div className="flex-1 mt-16 p-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {filteredPosts.map((post) => (
@@ -294,138 +289,88 @@ const Main = () => {
         </div>
       </div>
 
-      {/* Selected Post Modal */}
       {selectedPost && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-          <div className={`p-6 rounded-lg shadow-lg w-full max-w-lg ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-white text-gray-900'}`}>
-            <h2 className="text-xl font-bold mb-4">{selectedPost.title}</h2>
-            <p className="mb-4">{selectedPost.description}</p>
+        <div className={`fixed inset-0 ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-white text-gray-900'} flex items-center justify-center z-50 overflow-y-auto`}>
+          <div className={`relative w-full h-full max-w-none p-6 rounded-none shadow-none ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-white text-gray-900'}`}>
+            <button
+              onClick={() => setSelectedPost(null)}
+              className="absolute top-4 right-4 text-gray-500 hover:text-gray-300 transition-colors duration-300"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+
+            <h2 className="text-3xl font-bold mb-4">{selectedPost.title}</h2>
+            <p className="mb-4 text-gray-500">{selectedPost.description}</p>
             <div
-              className={`prose ${isDarkMode ? 'prose-invert' : ''}`}
+              className={`prose max-w-none ${isDarkMode ? 'prose-invert' : ''}`}
               dangerouslySetInnerHTML={{ __html: selectedPost.contentHtml }}
             />
-            <div className="flex justify-end mt-4">
-              <button
-                onClick={() => setSelectedPost(null)}
-                className="p-2 bg-red-600 hover:bg-red-500 text-white rounded-md transition-colors duration-300"
-              >
-                Fechar
-              </button>
-            </div>
           </div>
         </div>
       )}
 
-      {/* New Post Modal */}
-      {isNewPostModalOpen && (
+      {isPostModalOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-          <div className={`p-6 rounded-lg shadow-lg w-full max-w-lg ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-white text-gray-900'}`}>
-            <h2 className="text-xl font-bold mb-4">Criar Novo Post</h2>
+          <div className={`w-full h-full p-6 ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-white text-gray-900'}`}>
+            <h2 className="text-2xl font-bold mb-4">Criar Novo Post</h2>
             <input
               type="text"
               placeholder="Título"
-              value={newPost.title}
-              onChange={(e) => setNewPost({ ...newPost, title: e.target.value })}
+              value={post.title}
+              onChange={(e) => setPost({ ...post, title: e.target.value })}
               className={`block w-full p-2 mb-2 rounded-md transition-colors duration-300 ${isDarkMode ? 'bg-gray-700 text-white placeholder-gray-400' : 'bg-gray-200 text-gray-800 placeholder-gray-500'}`}
             />
             <input
               type="text"
               placeholder="Descrição"
-              value={newPost.description}
-              onChange={(e) => setNewPost({ ...newPost, description: e.target.value })}
+              value={post.description}
+              onChange={(e) => setPost({ ...post, description: e.target.value })}
               className={`block w-full p-2 mb-2 rounded-md transition-colors duration-300 ${isDarkMode ? 'bg-gray-700 text-white placeholder-gray-400' : 'bg-gray-200 text-gray-800 placeholder-gray-500'}`}
             />
-            <ReactQuill
-              value={newPost.contentHtml}
-              onChange={(value) => setNewPost({ ...newPost, contentHtml: value })}
-              className="mb-4"
-            />
+            <div className="h-96 overflow-y-auto mb-4">
+              <ReactQuill
+                value={post.contentHtml}
+                onChange={(value) => setPost({ ...post, contentHtml: value })}
+                className="h-full"
+              />
+            </div>
             <div className="flex justify-end">
               <button
-                onClick={() => setIsNewPostModalOpen(false)}
+                onClick={() => setIsPostModalOpen(false)}
                 className="p-2 bg-red-600 hover:bg-red-500 text-white rounded-md transition-colors duration-300 mr-2"
               >
                 Cancelar
               </button>
               <button
-                onClick={handleCreatePost}
+                onClick={post.id === 0 ? handleCreatePost : handleSaveEditedPost }
                 className="p-2 bg-blue-600 hover:bg-blue-500 text-white rounded-md transition-colors duration-300"
               >
-                Criar Post
+                { post.id === 0 ? 'Criar Post' : 'Salvar Alterações' }
               </button>
             </div>
           </div>
         </div>
       )}
 
-      {/* Edit Post Modal */}
-      {isEditModalOpen && postToEdit && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-          <div className={`p-6 rounded-lg shadow-lg w-full max-w-lg ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-white text-gray-900'}`}>
-            <h2 className="text-xl font-bold mb-4">Editar Post</h2>
-            <input
-              type="text"
-              placeholder="Título"
-              value={postToEdit.title}
-              onChange={(e) => setPostToEdit({ ...postToEdit, title: e.target.value })}
-              className={`block w-full p-2 mb-2 rounded-md transition-colors duration-300 ${isDarkMode ? 'bg-gray-700 text-white placeholder-gray-400' : 'bg-gray-200 text-gray-800 placeholder-gray-500'}`}
-            />
-            <input
-              type="text"
-              placeholder="Descrição"
-              value={postToEdit.description}
-              onChange={(e) => setPostToEdit({ ...postToEdit, description: e.target.value })}
-              className={`block w-full p-2 mb-2 rounded-md transition-colors duration-300 ${isDarkMode ? 'bg-gray-700 text-white placeholder-gray-400' : 'bg-gray-200 text-gray-800 placeholder-gray-500'}`}
-            />
-            <ReactQuill
-              value={postToEdit.contentHtml}
-              onChange={(value) => setPostToEdit({ ...postToEdit, contentHtml: value })}
-              className="mb-4"
-            />
-            <div className="flex justify-end">
-              <button
-                onClick={() => setIsEditModalOpen(false)}
-                className="p-2 bg-red-600 hover:bg-red-500 text-white rounded-md transition-colors duration-300 mr-2"
-              >
-                Cancelar
-              </button>
-              <button
-                onClick={handleSaveEditedPost}
-                className="p-2 bg-blue-600 hover:bg-blue-500 text-white rounded-md transition-colors duration-300"
-              >
-                Salvar
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
-
-      {/* Password Modals */}
-      {isPasswordModalForNewPostOpen && (
+      {(isPasswordModalForNewPostOpen || isPasswordModalForEditOpen || isPasswordModalForDeleteOpen) && (
         <PasswordModal
-          title="Digite a senha para criar um novo post"
-          onCancel={() => setIsPasswordModalForNewPostOpen(false)}
-          onSubmit={handlePasswordSubmitForNewPost}
-          passwordInput={passwordInput}
-          setPasswordInput={setPasswordInput}
-        />
-      )}
-
-      {isPasswordModalForEditOpen && (
-        <PasswordModal
-          title="Digite a senha para editar o post"
-          onCancel={() => setIsPasswordModalForEditOpen(false)}
-          onSubmit={handlePasswordSubmitForEdit}
-          passwordInput={passwordInput}
-          setPasswordInput={setPasswordInput}
-        />
-      )}
-
-      {isPasswordModalForDeleteOpen && (
-        <PasswordModal
-          title="Digite a senha para deletar o post"
-          onCancel={() => setIsPasswordModalForDeleteOpen(false)}
-          onSubmit={handlePasswordSubmitForDelete}
+          title={
+            `Digite a senha para ${isPasswordModalForNewPostOpen ? 'Criar Novo Post' : isPasswordModalForEditOpen ? 'Editar Post' : 'Deletar Post'}`
+          }
+          onCancel={() => {
+            if (isPasswordModalForNewPostOpen) setIsPasswordModalForNewPostOpen(false);
+            if (isPasswordModalForEditOpen) setIsPasswordModalForEditOpen(false);
+            if (isPasswordModalForDeleteOpen) setIsPasswordModalForDeleteOpen(false);
+          }}
+          onSubmit={
+            isPasswordModalForNewPostOpen
+              ? handlePasswordSubmitForNewPost
+              : isPasswordModalForEditOpen
+              ? handlePasswordSubmitForEdit
+              : handlePasswordSubmitForDelete
+          }
           passwordInput={passwordInput}
           setPasswordInput={setPasswordInput}
         />
